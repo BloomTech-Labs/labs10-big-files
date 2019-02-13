64ac94b5164b5a288ecc3b9da9c4aa0f5592d405
Anthony Greb <agreb255@gmail.com>
2019-02-13 18:09:38 -0500

    skeleton for create/edit page

---
+++ Frontend/share_big_files/src/views/createedit.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const FileName = styled.div`
+    width: 60%;
+    height: 40px;
+`;
+const SharedWithBox = styled.div`
+    width: 100%;
+    height: 200px;
+`;
+const UploadButtonHolder = styled.div`
+
+
+`;
+const ShareLinkHolder = styled.div`
+
+
+`;
+const VersionBrowserHolder = styled.div`
+
+
+`;
+const ConfirmButtons = styled.div`
+
+
+`;
+
+
+
+const CreateFile = () => {
+
+
+    render (
+        <>
+        <CreateFileHolder>
+            <FileName type="text" placeholder="Name"></FileName>
+            <SharedWithBox/>
+        </CreateFileHolder>
+        <UploadButtonHolder>
+            <button></button>
+            <Link to="/files">Upload File</Link>
+        </UploadButtonHolder>
+        <ShareLinkHolder>
+            <h3>Share Link:</h3>
+
+        </ShareLinkHolder>
+        <VersionBrowserHolder>
+            <h3>Version Browser</h3>
+            <button></button>
+            <button></button>
+        </VersionBrowserHolder>
+        <ConfirmButtons>
+            <button>Cancel</button>
+            <button>Save</button>
+        </ConfirmButtons>
+        </>
+    )
+}
+
+
+export default CreateFile;
