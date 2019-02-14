89afeb3f6f7eb515226b73207a5a093e254d1f76
thomaskisner <tjkisner@gmail.com>
2019-02-13 22:31:40 -0500

    Changed landingpage opacity and fixed div sizings, converted font to rems, created App styled-component

--- Frontend/share_big_files/src/App.js
+++ Frontend/share_big_files/src/App.js
@@ -4,19 +4,28 @@ import Landingview from "./views/landingview";
 // import {homeviewholder } from "./views/homeviewholder";
 // import {settingsholder} from './views/settingsholder';
 // import {billingholder} from './views/billingholder';
-import {Route} from "react-router-dom";
-import './App.css';
+import { Route } from "react-router-dom";
+import "./App.css";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  height: auto;
+  min-height: 100vh; 
+  width: 100%; 
+  margin: 0;
+  padding: 0;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Route exact path="/"             render={props =><Landingview {...props}/>}/>
-      {/* <Route exact path="/home/:files"  render={props =><createeditholder {...props}/>}/>
+      <AppContainer>
+        <Route exact path="/" render={props => <Landingview {...props} />} />
+        {/* <Route exact path="/home/:files"  render={props =><createeditholder {...props}/>}/>
       <Route exact path="/home/:new/edit/" render={props =><homeviewholder {...props}/>}/>
       <Route exact path="/home/:setting/" render={props =><settingsholder {...props}/>}/>
       <Route exact path="/home/:billing/" render={props =><billingholder {...props}/>}/> */}
-      </div>
+      </AppContainer>
     );
   }
 }
