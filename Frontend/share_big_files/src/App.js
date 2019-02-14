9fae8b12654f2b41dcf8e1d0e069cf84b58a69a1
K <kkhaag@gmail.com>
2019-02-14 08:31:51 -0800

    Added Stripe to App.js

--- Frontend/share_big_files/src/App.js
+++ Frontend/share_big_files/src/App.js
@@ -4,11 +4,7 @@ import Landingview from "./views/landingview";
 // import {homeviewholder } from "./views/homeviewholder";
 // import {settingsholder} from './views/settingsholder';
 // import {billingholder} from './views/billingholder';
-<<<<<<< HEAD
-import Stripe from "./components/StripeFE" //
-import {Route} from "react-router-dom";
-import './App.css';
-=======
+import Stripe from './components/StripeFE'
 import { Route } from "react-router-dom";
 import "./App.css";
 import styled from "styled-components";
@@ -20,21 +16,14 @@ const AppContainer = styled.div`
   margin: 0;
   padding: 0;
 `;
->>>>>>> 9d5d616a3d8bcde6281b35c2fa3bf18af5476439
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-      <Route exact path="/"             render={props =><Landingview {...props}/>}/>
-      <Route path="/stripe" render={props =><Stripe {...props} />} />
-      {/* <Route exact path="/home/:files"  render={props =><createeditholder {...props}/>}/>
-=======
       <AppContainer>
         <Route exact path="/" render={props => <Landingview {...props} />} />
+        <Route path="/stripe" render={props =><Stripe {...props} />} />
         {/* <Route exact path="/home/:files"  render={props =><createeditholder {...props}/>}/>
->>>>>>> 9d5d616a3d8bcde6281b35c2fa3bf18af5476439
       <Route exact path="/home/:new/edit/" render={props =><homeviewholder {...props}/>}/>
       <Route exact path="/home/:setting/" render={props =><settingsholder {...props}/>}/>
       <Route exact path="/home/:billing/" render={props =><billingholder {...props}/>}/> */}
