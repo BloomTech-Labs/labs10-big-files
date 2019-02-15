0aaafea3d603da795fd27b004b92f9d10c7f5993
thomaskisner <tjkisner@gmail.com>
2019-02-15 12:25:37 -0500

    added react-icons, implement icons in add file view, further styled create/edit implements icons

--- Frontend/share_big_files/src/views/createeditholder.js
+++ Frontend/share_big_files/src/views/createeditholder.js
@@ -4,9 +4,10 @@ import LeftMenu from "./leftmenu";
 import CreateFile from "./createedit";
 import styled from "styled-components";
 
+
 const CreateEditDiv = styled.div`
   display: flex;
-  height:500px;
+ 
 `;
 
 const CreateEditHolder = () => {
