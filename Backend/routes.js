b953311763596c356ace63c5f2db97f36f5a5ac7
K <kkhaag@gmail.com>
2019-02-16 15:24:59 -0800

    commented out everything related to Stripe

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,5 +1,5 @@
-const paymentApi = require("./payment");
-const configureRoutes = server => {
-  paymentApi(server);
-};
-module.exports = configureRoutes;
+// const paymentApi = require("./payment");
+// const configureRoutes = server => {
+//   paymentApi(server);
+// };
+// module.exports = configureRoutes;
--- Backend/server.js
+++ Backend/server.js
@@ -9,8 +9,8 @@ const server = express()
 server.use(cors())
 server.use(bodyParser.json())
 
-const configureRoutes = require("./routes")
-configureRoutes(server);
+// const configureRoutes = require("./routes")
+// configureRoutes(server);
 
-server.listen(process.env.PORT || 8001, 
-    () => {console.log(`listening on port ${process.env.PORT}`)})
+server.listen(8000, 
+    () => {console.log(`listening on port 8000`)})
