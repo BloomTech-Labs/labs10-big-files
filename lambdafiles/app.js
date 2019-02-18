867509f78bd6207793b27da30890022ccfb1556d
James Cassidy <cassidyjamesw@gmail.com>
2019-02-18 17:47:58 -0500

    moved user id get

--- lambdafiles/app.js
+++ lambdafiles/app.js
@@ -1,85 +1,79 @@
-const express = require('express');
+const express = require("express");
 // const knex = require('knex');
-const pg = require('pg');
-const helmet = require('helmet');
-const forever = require('forever');
-require('dotenv').config();
+const pg = require("pg");
+const helmet = require("helmet");
+const forever = require("forever");
+require("dotenv").config();
 
 var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+  http = require("http"),
+  fs = require("fs"),
+  html = fs.readFileSync("index.html");
 
 const server = express();
 server.use(express.json());
 
 // const db = knex(knexConfig.development);
 
-server.get('/', (req, res) => {
-    res.send('Hello, world');
-});
-
+// ?
 const conString = process.env.con_string;
-
 var client = new pg.Client(conString);
 client.connect();
 
-server.get("/users/:id", async (req, res) => {
-    const {id} = req.params;
-    client.query(`SELECT * FROM users WHERE users.user_id = ${id}`)
-	.then(result => {
-	    res.status(200).json(result.rows);
-	    // process.exit();
-	})
-	.catch(e => {
-	    res.status(404).json(e.stack);
-	})
-	// .then(() => client.end());
-});
-
-server.get('/users', async (req, res) => {
-    client.query(`SELECT * FROM users`)
-	.then(result => {
-	    res.status(200).json(result.rows);
-	    // process.exit();
-	})
-	.catch(e => {
-	    res.status(404).json(e.stack);
-	})
-	// .then(() => client.end());
+server.get("/users", async (req, res) => {
+  client
+    .query(`SELECT * FROM users`)
+    .then(result => {
+      res.status(200).json(result.rows);
+      // process.exit();
+    })
+    .catch(e => {
+      res.status(404).json(e.stack);
+    });
+  // .then(() => client.end());
 });
 
 server.post("/users", (request, res) => {
-    console.log("RB", request.body);
-    const { username, paid, logged_in, email, created_at, last_login } = request.body;
-    client.query(`INSERT INTO users (
+  console.log("RB", request.body);
+  const {
+    username,
+    paid,
+    logged_in,
+    email,
+    created_at,
+    last_login
+  } = request.body;
+  client
+    .query(
+      `INSERT INTO users (
     username, paid, logged_in, email, created_at, last_login)
-    VALUES ($1, $2, $3, $4, $5, $6)`,[username, paid, logged_in, email, created_at, last_login ])
-	.then(result => {
-	    res.status(200).json(result);
-	    // process.exit();
-	})
-	.catch(e => {
-	    console.error(e.detail),
-	    res.send(e)
-	})
-	// .then(() => client.end())
+    VALUES ($1, $2, $3, $4, $5, $6)`,
+      [username, paid, logged_in, email, created_at, last_login]
+    )
+    .then(result => {
+      res.status(200).json(result);
+      // process.exit();
+    })
+    .catch(e => {
+      console.error(e.detail), res.send(e);
+    });
+  // .then(() => client.end())
 });
 
 server.delete("/delete/:id", (request, res) => {
-    const userID = parseInt(request.params.id);
-    client.query('DELETE FROM users WHERE user_id = $1', [userID])
-	.then(result => {
-	    res.status(200).json(result);
-	})
-	.catch(e => {
-	    console.error(e.detail), 
-	    res.send(e)
-	})
+  const userID = parseInt(request.params.id);
+  client
+    .query("DELETE FROM users WHERE user_id = $1", [userID])
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(e => {
+      console.error(e.detail), res.send(e);
+    });
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
 
 // Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+console.log("Server running at http://127.0.0.1:" + port + "/");
--- lambdafiles/routes/api/users.js
+++ lambdafiles/routes/api/users.js
@@ -6,3 +6,17 @@ module.exports = router;
 server.get("/", (req, res) => {
   res.send("Hello, world");
 });
+
+server.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  client
+    .query(`SELECT * FROM users WHERE users.user_id = ${id}`)
+    .then(result => {
+      res.status(200).json(result.rows);
+      // process.exit();
+    })
+    .catch(e => {
+      res.status(404).json(e.stack);
+    });
+  // .then(() => client.end());
+});
