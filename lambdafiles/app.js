22408183712775a03487dcaa2a87e5446d5976a5
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-02-18 12:25:41 -0800

    POST and DELETE are working with postgres

--- lambdafiles/app.js
+++ lambdafiles/app.js
@@ -29,145 +29,57 @@ server.get("/users/:id", async (req, res) => {
     client.query(`SELECT * FROM users WHERE users.user_id = ${id}`)
 	.then(result => {
 	    res.status(200).json(result.rows);
-	    process.exit();
+	    // process.exit();
 	})
 	.catch(e => {
 	    res.status(404).json(e.stack);
 	})
-	.then(() => client.end());
+	// .then(() => client.end());
 });
 
 server.get('/users', async (req, res) => {
     client.query(`SELECT * FROM users`)
 	.then(result => {
 	    res.status(200).json(result.rows);
-	    process.exit();
+	    // process.exit();
 	})
 	.catch(e => {
 	    res.status(404).json(e.stack);
 	})
-	.then(() => client.end());
+	// .then(() => client.end());
 });
 
-
-// const createUser = (request, response) => {
-//     const { username, email } = request.body;
-//     client.query('INSERT INTO users (username, email) VALUES ($1, $2)', [username, email], (error, res) => {
-// 	if (error) {
-// 	    throw error;
-// 	}
-// 	res.status(200).json(res.rows);
-//     });
-// };
-
-server.post('/users', async (req, res) => {
-    const {username, user_id, paid, logged_in, email, created_at, last_login} = req.params;
-    //req.params or req.body?
-    client.query(`INSERT INTO users(username, user_id, paid, logged_in, email, created_at, last_login) VALUES($1, $2, $3, $4, $5, $6, $7`, [username, user_id, paid, logged_in, email, created_at, last_login])
+server.post("/users", (request, res) => {
+    console.log("RB", request.body);
+    const { username, paid, logged_in, email, created_at, last_login } = request.body;
+    client.query(`INSERT INTO users (
+    username, paid, logged_in, email, created_at, last_login)
+    VALUES ($1, $2, $3, $4, $5, $6)`,[username, paid, logged_in, email, created_at, last_login ])
 	.then(result => {
-	    res.status(200).json(result.rows);
-	    process.exit();
+	    res.status(200).json(result);
+	    // process.exit();
 	})
 	.catch(e => {
-	    res.status(404).json(e.stack);
-	    console.log(e.stack);
-	    process.exit();
+	    console.error(e.detail),
+	    res.send(e)
 	})
-	.then(() => client.end());
+	// .then(() => client.end())
 });
 
-
-// change the request itself?
-// {
-//     "username": "RonaldMcDonald",
-//     "email": "Ronmcd@lambdaschool.com"
-// }
-
-
-
-
-// server.post('/users', async (req, res) => {
-//     const {username} = req.params;
-//     const {email} = req.params;
-//     client.query(`INSERT INTO users(username, user_id, paid, logged_in, email, created_at, last_login) VALUES($1, 4, false, false, $2, null, null`, [username, email])
-// 	.then(result => {
-// 	    res.status(200).json(result.rows);
-// 	    process.exit();
-// 	})
-// 	.catch(e => {
-// 	    res.status(404).json(e.stack);
-// 	    console.log(e.stack);
-// 	    process.exit();
-// 	})
-// 	.then(() => client.end());
-// });
-
-// client.query('SELECT * FROM users WHERE users.user_id = "1"')
-
-
-
-
-
-
-
-
-// let users = [
-//     {
-// 	name: 'Akshay Gadkari',
-// 	email: 'akshay.gadkari@lambdaschool.com',
-// 	id: 0,
-// 	paying: 0
-//     },
-//     {
-// 	name: 'John Doe',
-// 	email: 'jdoe@lambdaschool.com',
-// 	id: 1,
-// 	paying: 1
-//     }
-// ];
-
-// server.get('/api/users', (req, res) => {
-//     db('users').then(users => {
-// 	res.status(200).json(users);
-//     })
-// 	.catch(err => res.status(500).json(err));
-// });
-
-// let nextId = 2;
-// server.post('/users', (req, res) => {
-//     console.log(req.body);
-//     const user = req.body;
-//     user.id = 1 + users.length;
-//     users.filter(n => n);
-//     user.paying = 0;
-//     users.push(user);
-//     res.status(201).json(users);
-// });
-
-// server.put('/users/:id', (req, res) => {
-//     const user = users.find(u => u.id == req.params.id);
-//     if(!user) {
-// 	res.status(404).json({message: 'User doesn\'t exist'});
-//     }
-//     else {
-// 	Object.assign(user, req.body);
-// 	res.status(200).json(user);
-//     }
-// });
-
-// server.get('/users/:id', (req, res) => {
-//     const user = users.find(u => u.id == req.params.id);
-//     if(!user) {
-// 	res.status(404).json({message: 'User doesn\'t exist'});
-//     }
-//     else {
-// 	res.json(user);
-//     }
-// });
+server.delete("/delete/:id", (request, res) => {
+    const userID = parseInt(request.params.id);
+    client.query('DELETE FROM users WHERE user_id = $1', [userID])
+	.then(result => {
+	    res.status(200).json(result);
+	})
+	.catch(e => {
+	    console.error(e.detail), 
+	    res.send(e)
+	})
+});
 
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
 
 // Put a friendly message on the terminal
 console.log('Server running at http://127.0.0.1:' + port + '/');
-
