77c5461450e4edd9ac3ecd23b19ffad3bd7bab24
James Cassidy <cassidyjamesw@gmail.com>
2019-02-18 17:59:57 -0500

    cleaned app.js

--- lambdafiles/api/app.js
+++ lambdafiles/api/app.js
@@ -4,12 +4,16 @@ const pg = require("pg");
 const helmet = require("helmet");
 const forever = require("forever");
 require("dotenv").config();
+//Routes
+const userRoute = require("../routes/api/users");
+const s3Route = require("../routes/api/s3");
 
 var port = process.env.PORT || 3000,
   http = require("http"),
   fs = require("fs"),
   html = fs.readFileSync("index.html");
 
+// server
 const server = express();
 server.use(express.json());
 
@@ -20,8 +24,8 @@ const conString = process.env.con_string;
 var client = new pg.Client(conString);
 client.connect();
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+//routes
+server.use("/api/users/", userRoute);
+server.use("/api/s3/", s3Route);
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:" + port + "/");
+module.exports = server;
