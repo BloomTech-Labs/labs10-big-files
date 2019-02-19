1d04ea64e07f52688b398544efff74d73fc6d0d9
James Cassidy <cassidyjamesw@gmail.com>
2019-02-19 12:41:12 -0500

    comments

--- lambdafiles/routes/api/s3.js
+++ lambdafiles/routes/api/s3.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const router = express.Router();
 const aws = require("aws-sdk");
 const multerS3 = require("multer-s3");
 const multer = require("multer");
 const path = require("path");
+/**
+ * express.Router() creates modular, mountable route handlers
+ * A Router instance is a complete middleware and routing system; for this reason, it is often referred to as a “mini-app”.
+ */
+const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello, world");
