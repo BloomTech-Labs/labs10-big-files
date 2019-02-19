1a656a9bb9e6203dd5644bf94b8b9f2c6ca9579f
James Cassidy <cassidyjamesw@gmail.com>
2019-02-19 12:40:21 -0500

    requirements

--- lambdafiles/routes/api/s3.js
+++ lambdafiles/routes/api/s3.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+const multer = require("multer");
+const path = require("path");
 
 router.get("/", (req, res) => {
   res.send("Hello, world");
