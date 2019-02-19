d8fb095439010dd4b9ad4654f5b59ac5876e080a
James Cassidy <cassidyjamesw@gmail.com>
2019-02-19 16:19:04 -0500

    get object from s3

--- lambdafiles/routes/api/s3.js
+++ lambdafiles/routes/api/s3.js
@@ -11,37 +11,37 @@ const router = express.Router();
 
 // Using AWS-SDK to set up profile
 const s3 = new aws.S3({
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
-    Bucket: process.env.Bucket
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  Bucket: process.env.Bucket
 });
 
 //Single File Upload
 const Bucket = process.env.Bucket;
 
 const fileUpload = multer({
-    storage: multerS3({
-	s3: s3,
-	bucket: "s3lambdafiles123",
-	// acl: "public-read",
-	key: function(req, file, cb) {
-	    cb(
-		null,
-		path.basename(file.originalname, path.extname(file.originalname)) +
-		    "-" +
-		    Date.now() +
-		    path.extname(file.originalname)
-	    );
-	}
-	// key: function (req, file, cb) {
-        //     console.log(file);
-        //     cb(null, file.originalname); //use Date.now() for unique file keys
-        // }
-    }),
-    limits: { fileSize: 2000000 } // In bytes: 2000000 bytes = 2 MB
-    // fileFilter: function(req, file, cb) {
-    // 	checkFileType(file, cb);
+  storage: multerS3({
+    s3: s3,
+    bucket: "s3lambdafiles123",
+    // acl: "public-read",
+    key: function(req, file, cb) {
+      cb(
+        null,
+        path.basename(file.originalname, path.extname(file.originalname)) +
+          "-" +
+          Date.now() +
+          path.extname(file.originalname)
+      );
+    }
+    // key: function (req, file, cb) {
+    //     console.log(file);
+    //     cb(null, file.originalname); //use Date.now() for unique file keys
     // }
+  }),
+  limits: { fileSize: 2000000 } // In bytes: 2000000 bytes = 2 MB
+  // fileFilter: function(req, file, cb) {
+  // 	checkFileType(file, cb);
+  // }
 }).single("fileUpload");
 
 // TEST ROUTE
@@ -51,29 +51,40 @@ router.get("/", (req, res) => {
 
 // ROUTE TO UPLOAD FILE
 router.post("/files", (req, res) => {
-    fileUpload(req, res, error => {
-	// console.log( 'requestOkokok', req.file );
-	// console.log( 'error', error );
-	if (error) {	    
-	    console.log("errors:", error);
-	    res.json({ error: error });
-	} else {
-	    // If File not found
-	    if (req.file === undefined) {
-		console.log("Error: No File Selected!");
-		res.json("Error: No File Selected");
-	    } else {
-		// If Success
-		const imageName = req.file.key;
-		const imageLocation = req.file.location;
-		// Save the file name into database into profile model
-		res.json({
-		    image: imageName,
-		    location: imageLocation
-		});
-	    }
-	}
-    });
+  fileUpload(req, res, error => {
+    // console.log( 'requestOkokok', req.file );
+    // console.log( 'error', error );
+    if (error) {
+      console.log("errors:", error);
+      res.json({ error: error });
+    } else {
+      // If File not found
+      if (req.file === undefined) {
+        console.log("Error: No File Selected!");
+        res.json("Error: No File Selected");
+      } else {
+        // If Success
+        const imageName = req.file.key;
+        const imageLocation = req.file.location;
+        // Save the file name into database into profile model
+        res.json({
+          image: imageName,
+          location: imageLocation
+        });
+      }
+    }
+  });
+});
+
+// Retrieves an object from an S3 Bucket
+var params = {
+  Bucket: "examplebucket",
+  Key: "hello.txt"
+};
+s3.getObject(params, function(err, data) {
+  if (err) console.log(err, err.stack);
+  // an error occurred
+  else console.log(data); // successful response
 });
 
 module.exports = router;
