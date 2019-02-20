7517a8a506e5565ba06827968667040cca3b2166
James Cassidy <cassidyjamesw@gmail.com>
2019-02-20 10:50:32 -0500

    SES skeleton

---
+++ lambdafiles/routes/api/ses.js
@@ -0,0 +1,32 @@
+// docs for SES
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SES.html#sendEmail-property
+
+var aws = require("aws-sdk");
+app.get("/", (req, res) => {
+  var ses = new aws.SES();
+  var to = ["yourverifiedemail@email.com"];
+  var from = "yourverifiedemail@email.com";
+  ses.sendEmail(
+    {
+      Source: from,
+      Destination: { ToAddresses: to },
+      Message: {
+        Subject: {
+          Data: "Email from Amazon SES"
+        },
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: "Hey, It's a Email from amazon SES(Simple Email Service)"
+          }
+        }
+      }
+    },
+    function(err, data) {
+      if (err) throw err;
+      console.log("Email sent:");
+      console.log(data);
+      return false;
+    }
+  );
+});
