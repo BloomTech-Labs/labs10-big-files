086f00f21ff0064700d0788391b18490290a217b
thomaskisner <tjkisner@gmail.com>
2019-02-20 16:10:01 -0500

    set up Auth0 to require a username on signup, deleted unused imports in various files

--- Frontend/share_big_files/src/views/settings.js
+++ Frontend/share_big_files/src/views/settings.js
@@ -1,35 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
 const SettingsDiv = styled.div`
-    margin-left: 2%;
+  margin-left: 2%;
 `;
 
-function SetPassword() {
-    const [name, setName] = useState(null);
-    const [oldPassword, setOldPassword] = useState(oldPassword);
-    const [newPassword, setNewPassword] = useState(newPassword);
-    // const [newPassword, setNewPassword] = useState(null);
-
-useEffect(() => {
-      setName(this.namefield);
-    document.oldPassword = setOldPassword;
-    document.newPassword = setNewPassword;
-})
-}
 ///going to need hooks for input state
-const Settings = props =>{
-    return(
-        <SettingsDiv>
-            
-            <p>name</p> <input type="text" value={props.name} onChange={props.useEffect}/>
-            <p>Old Password</p> <input type="text" value={props.oldPassword} onChange={props.useEffect}/>
-            <p>New Password</p> <input type="text" value={props.newPassword} onChange={props.useEffect}/>
-            <p>New Password</p> <input type="text" value={props.newPassword} onChange={props.useEffect}/>
-            <button onClick={() => SetPassword()}>Save</button>
-        </SettingsDiv>
-    )
-}
-
+const Settings = props => {
+    function SetPassword() {
+        const [name, setName] = useState(null);
+        const [oldPassword, setOldPassword] = useState(oldPassword);
+        const [newPassword, setNewPassword] = useState(newPassword);
+        // const [newPassword, setNewPassword] = useState(null);
+      }
+    
+      useEffect(() => {
+          console.log('in useEffect');
+        axios
+          .get(
+            `http://lambdafiles.us-east-2.elasticbeanstalk.com/api/users/users/609`
+          )
+          .then(response => {
+            console.log(response);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+    
+      });  
+  return (
+    <SettingsDiv>
+      <p>name</p>{" "}
+      <input type="text" value={props.name} onChange={props.useEffect} />
+      <p>Old Password</p>{" "}
+      <input type="text" value={props.oldPassword} onChange={props.useEffect} />
+      <p>New Password</p>{" "}
+      <input type="text" value={props.newPassword} onChange={props.useEffect} />
+      <p>New Password</p>{" "}
+      <input type="text" value={props.newPassword} onChange={props.useEffect} />
+      <button onClick={() => SetPassword()}>Save</button>
+    </SettingsDiv>
+  );
+};
 
 export default Settings;
