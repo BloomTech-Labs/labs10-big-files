c7d6b26af491045ca501d6801ab1fe792f3ff414
thomaskisner <tjkisner@gmail.com>
2019-02-21 00:05:03 -0500

    conditional render working locally

--- Frontend/share_big_files/src/views/billing.js
+++ Frontend/share_big_files/src/views/billing.js
@@ -78,7 +78,7 @@ const Billing = () => {
         console.log(isPro)
       })
       .catch(err => console.log(err));
-  }, []);
+  } );
   const [billing, setBilling] = useState(null);
   const [isPro, setIsPro] = useState(null);
 
