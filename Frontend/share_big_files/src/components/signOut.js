5ad32dc2f93930414b03eec560e05bcb9d0ea365
thomaskisner <tjkisner@gmail.com>
2019-02-22 13:11:18 -0500

    Fixed signout button, clear cookies and history push are now in a promise which, executes before the window reload

--- Frontend/share_big_files/src/components/signOut.js
+++ Frontend/share_big_files/src/components/signOut.js
@@ -2,24 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import history from "../history";
 
-function signOutHandler(){
-console.log('Sign out handler fired')
-localStorage.clear('accessToken');
-localStorage.clear('expiresAt');
-history.push("/add");
-window.location.reload();
+function signOutHandler() {
+  var signoutPromise = new Promise(function(resolve, error) {
+    console.log("Sign out handler fired");
+    localStorage.clear("accessToken");
+    localStorage.clear("expiresAt");
+    history.push("/");
+  });
+  signoutPromise.then(window.location.reload());
 }
 
 const SignOutButton = styled.button`
-height:2rem;
+  height: 2rem;
 `;
 
 const SignOut = props => {
-    return (
-    <SignOutButton onClick={signOutHandler}>
-        Sign Out
-    </SignOutButton>
-    )
-}
+  return <SignOutButton onClick={signOutHandler}>Sign Out</SignOutButton>;
+};
 
 export default SignOut;
