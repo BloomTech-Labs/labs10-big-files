380bcd7f80b3c569dcfccfc5fbc80d62f0468c18
Anthony Greb <agreb255@gmail.com>
2019-02-25 15:48:21 -0500

    working on responsivness

--- Frontend/share_big_files/src/views/billingholder.js
+++ Frontend/share_big_files/src/views/billingholder.js
@@ -2,16 +2,27 @@ import React from "react";
 import NavHeader from "./navheader";
 import LeftMenu from "./leftmenu";
 import Billing from "./billing";
+import media from "../styledComponents/media.js";
 import styled from "styled-components";
 
-const BillingPage = styled.div`
+const BillingPage = styled(props => <div {...props} />)`
   width: 100%;
   height: auto;
   min-height: 100vh;
   margin: 0 auto;
   background-image: url("http://get.addonreviews.com/cmsimages/lp/fileshare/cloudbackground.jpg");
   background-size: 100% 100%;
+  ${media.phone`
+    
+`}
+  ${media.tablet`
+
+`}
+  ${media.desktop`
+
+`}
 `;
+
 
 
 const MenuBillingDiv = styled.div`
