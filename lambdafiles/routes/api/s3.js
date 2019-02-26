3cb451af23e8adc3021c089ace92eb76f543a197
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-02-26 10:27:22 -0800

    fixed delete in s3

--- lambdafiles/routes/api/s3.js
+++ lambdafiles/routes/api/s3.js
@@ -63,43 +63,43 @@ const fileUpload = multer({
 }).single("fileUpload");
 
 // ROUTE TO UPLOAD FILE
-router.post("/files", (req, res) => {
-  console.log("REQ", req);
-  console.log("REQ_BODY", req.body);
-  fileUpload(req, res, error => {
-    // console.log( 'requestOkokok', req.file );
-    // console.log( 'error', error );
-    if (error) {
-      console.log("errors:", error);
-      res.json({ error: error });
-    } else {
-      // If File not found
-      if (req.file === undefined) {
-        console.log("Error: No File Selected!");
-        res.json("Error: No File Selected");
-      } else {
-        // If Success
-        const url = req.file.location;
-        // const fk_user_id = 3;
-        client
-          .query(`INSERT INTO files (url, fk_user_id) VALUES ($1, $2)`, [
-            url,
-            fk_user_id
-          ])
-          .then(result => {
-            res.status(200).json(result);
-          })
-          .catch(e => {
-            console.error(e), res.send(e);
-          });
-        // Save the file name into database into profile model
-        // res.json({
-        //     url
-        // });
-      }
-    }
-  });
-});
+// router.post("/files", (req, res) => {
+//   console.log("REQ", req);
+//   console.log("REQ_BODY", req.body);
+//   fileUpload(req, res, error => {
+//     // console.log( 'requestOkokok', req.file );
+//     // console.log( 'error', error );
+//     if (error) {
+//       console.log("errors:", error);
+//       res.json({ error: error });
+//     } else {
+//       // If File not found
+//       if (req.file === undefined) {
+//         console.log("Error: No File Selected!");
+//         res.json("Error: No File Selected");
+//       } else {
+//         // If Success
+//         const url = req.file.location;
+//         // const fk_user_id = 3;
+//         client
+//           .query(`INSERT INTO files (url, fk_user_id) VALUES ($1, $2)`, [
+//             url,
+//             fk_user_id
+//           ])
+//           .then(result => {
+//             res.status(200).json(result);
+//           })
+//           .catch(e => {
+//             console.error(e), res.send(e);
+//           });
+//         // Save the file name into database into profile model
+//         // res.json({
+//         //     url
+//         // });
+//       }
+//     }
+//   });
+// });
 
 router.get('/files', (req, res) => {
     client.query(`SELECT * FROM files`)
@@ -110,6 +110,21 @@ router.get('/files', (req, res) => {
     .catch(e => {
         console.error(e), res.send(e);
     });
+});
+
+router.post("/files/id", (request, res) => {
+    console.log("RB", request.body);
+    const { fk_user_id, filename } = request.body;
+    client.query(
+	`INSERT INTO files (fk_user_id, filename) VALUES ($1, $2) RETURNING file_id`, [fk_user_id, filename])
+	.then(result => {
+	    res.status(200).json(result.rows);
+	    // process.exit();
+	})
+	.catch(e => {
+	    console.error(e.detail), res.send(e);
+	});
+    // .then(() => client.end())
 });
 
 // (POST FK —> PUT URL)
@@ -147,6 +162,8 @@ router.put("/files", (req, res) => {
 	    }
 	}
     });
+});
+
 /**
  *
  * PAID USER FILES
@@ -173,106 +190,61 @@ const paidFileUpload = multer({
 }).single("fileUpload");
 
 // ROUTE TO UPLOAD FILE
-router.post("/paidfiles", (req, res) => {
-  console.log("REQ", req);
-  console.log("REQ_BODY", req.body);
-  paidFileUpload(req, res, error => {
-    if (error) {
-      console.log("errors:", error);
-      res.json({ error: error });
-    } else {
-      // If File not found
-      if (req.file === undefined) {
-        console.log("Error: No File Selected!");
-        res.json("Error: No File Selected");
-      } else {
-        // If Success
-        const url = req.file.location;
-        client
-          .query(`INSERT INTO files (url, fk_user_id) VALUES ($1, $2)`, [
-            url,
-            fk_user_id
-          ])
-          .then(result => {
-            res.status(200).json(result);
-          })
-          .catch(e => {
-            console.error(e), res.send(e);
-          });
-      }
-    }
-  });
+router.post("/paidfiles/id", (request, res) => {
+    console.log("RB", request.body);
+    const { fk_user_id, filename } = request.body;
+    client.query(
+	`INSERT INTO files (fk_user_id, filename) VALUES ($1, $2) RETURNING file_id`, [fk_user_id, filename])
+	.then(result => {
+	    res.status(200).json(result.rows);
+	    // process.exit();
+	})
+	.catch(e => {
+	    console.error(e.detail), res.send(e);
+	});
+    // .then(() => client.end())
+});
+
+router.put("/paidfiles/", (req, res) => {
+    console.log("REQ", req);
+    console.log("REQ_BODY", req.body);
+    paidFileUpload(req, res, error => {
+	if (error) {
+	    console.log("errors:", error);
+	    res.json({ error: error });
+	} else {
+	    // If File not found
+	    if (req.file === undefined) {
+		console.log("Error: No File Selected!");
+		res.json("Error: No File Selected");
+	    } else {
+		client.query(`UPDATE files SET url = '${req.file.location}' WHERE file_id = (select MAX(file_id) FROM files) `)
+		    .then(result => {
+			res.status(200).json(result);
+		    })
+		    .catch(e => {
+			console.error(e), res.send(e);
+		    });
+	    }
+	}
+    });
 });
 
 // DELETE ROUTE
-router.delete("/files", (req, res) => {
-  const fileID = req.params.id;
-  var params = {
-    Bucket: "s3lambdafiles123",
-    Key: "file.txt"
-  };
-  s3.deleteObject(params, function(err, data) {
-    if (err) {
-      console.log(err, err.stack);
-      console.log("hi");
-      return res.send({ error: err });
-    } else {
-      console.log(data);
-      return res.send({ completed: "done" });
-    }
-  });
+router.delete("/files/delete/:id", (req, res) => {
+    const fileID = req.params.id;
+    // var params = {
+    //   Bucket: "s3lambdafiles123",
+    //   Key: "file.txt"
+    // };
+    client
+	.query("DELETE FROM files WHERE file_id = $1", [fileID])
+	.then(result => {
+	    res.status(200).json(result);
+	})
+	.catch(e => {
+	    console.error(e.detail), res.send(e);
+	});
 });
 
 module.exports = router;
-
-// NOTES
-
-router.post("/files/id", (request, res) => {
-	console.log("RB", request.body);
-    const { fk_user_id, filename } = request.body;
-	client.query(
-	    `INSERT INTO files (fk_user_id, filename) VALUES ($1, $2) RETURNING file_id`, [fk_user_id, filename])
-	  .then(result => {
-		res.status(200).json(result.rows);
-		// process.exit();
-	  })
-	  .catch(e => {
-		console.error(e.detail), res.send(e);
-	  });
-	// .then(() => client.end())
-  });
-
-
-// router.put("/files/id", (req, res) => {
-//     const {fk_user_id} = req.params;
-//     client.query(`UPDATE files SET fk_user_id = ${fk_user_id} WHERE file_id = (select MAX(file_id) FROM files) VALUES ($1)`, [fk_user_id])
-//         .then(result => {
-//         res.status(200).json(result);
-//     })
-//     .catch(e => {
-//         console.error(e), res.send(e);
-//     });
-// });
-
-// Retrieves a URL from an S3 Bucket
-// router.get('/files', (req, res) => {
-//     var params = {
-// 	Bucket: "s3lambdafiles123",
-// 	Key: "file.txt"
-//     };
-//     s3.getObject(params, function(err, data) {
-// 	const imageLocation = req.file.location;
-// 	if (err) {
-// 	    console.log(err, err.stack);
-// 	    console.log("hi");
-// 	    return res.send({ "error": err });
-// 	}
-// 	// an error occurred
-// 	else {
-// 	    console.log(data);
-// 	    res.json({
-// 		location: imageLocation
-// 	    });
-// 	} // successful response
-//     });
-// });
