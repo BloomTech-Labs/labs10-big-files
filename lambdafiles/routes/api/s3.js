455a5905e94216c5a24e959f9b96e8305c3f866f
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-02-26 11:28:06 -0800

    added GET request for specific file ID

--- lambdafiles/routes/api/s3.js
+++ lambdafiles/routes/api/s3.js
@@ -50,56 +50,9 @@ const fileUpload = multer({
           path.extname(file.originalname)
       );
     }
-    // key: function (req, file, cb) {
-    //     console.log(file);
-    //     cb(null, file.originalname); //use Date.now() for unique file keys
-    // }
   }),
-
   limits: { fileSize: 2000000 } // In bytes: 2000000 bytes = 2 MB
-  // fileFilter: function(req, file, cb) {
-  // 	checkFileType(file, cb);
-  // }
 }).single("fileUpload");
-
-// ROUTE TO UPLOAD FILE
-// router.post("/files", (req, res) => {
-//   console.log("REQ", req);
-//   console.log("REQ_BODY", req.body);
-//   fileUpload(req, res, error => {
-//     // console.log( 'requestOkokok', req.file );
-//     // console.log( 'error', error );
-//     if (error) {
-//       console.log("errors:", error);
-//       res.json({ error: error });
-//     } else {
-//       // If File not found
-//       if (req.file === undefined) {
-//         console.log("Error: No File Selected!");
-//         res.json("Error: No File Selected");
-//       } else {
-//         // If Success
-//         const url = req.file.location;
-//         // const fk_user_id = 3;
-//         client
-//           .query(`INSERT INTO files (url, fk_user_id) VALUES ($1, $2)`, [
-//             url,
-//             fk_user_id
-//           ])
-//           .then(result => {
-//             res.status(200).json(result);
-//           })
-//           .catch(e => {
-//             console.error(e), res.send(e);
-//           });
-//         // Save the file name into database into profile model
-//         // res.json({
-//         //     url
-//         // });
-//       }
-//     }
-//   });
-// });
 
 router.get('/files', (req, res) => {
     client.query(`SELECT * FROM files`)
@@ -111,6 +64,20 @@ router.get('/files', (req, res) => {
         console.error(e), res.send(e);
     });
 });
+
+
+router.get('/files/:id', (req, res) => {
+    const fileID = req.params.id;
+    client.query(`SELECT * FROM files WHERE file_id = ${fileID}`)
+        .then(result => {
+        res.status(200).json(result.rows);
+        //console.log(`works ${fk_user_id}`);
+    })
+    .catch(e => {
+        console.error(e), res.send(e);
+    });
+});
+
 
 router.post("/files/id", (request, res) => {
     console.log("RB", request.body);
@@ -130,11 +97,9 @@ router.post("/files/id", (request, res) => {
 // (POST FK —> PUT URL)
 // ROUTE TO UPLOAD FILE
 router.put("/files", (req, res) => {
-	console.log("REQ", req)
-	console.log("REQ_BODY", req.body)
+    console.log("REQ", req);
+    console.log("REQ_BODY", req.body);
     fileUpload(req, res, error => {
-	// console.log( 'requestOkokok', req.file );
-	// console.log( 'error', error );
 	if (error) {
 	    console.log("errors:", error);
 	    res.json({ error: error });
@@ -144,21 +109,13 @@ router.put("/files", (req, res) => {
 		console.log("Error: No File Selected!");
 		res.json("Error: No File Selected");
 	    } else {
-		// If Success
-		// const url = req.file.location;
-		// const fk_user_id = 3;
-		// client.query(`UPDATE files SET url = 'abvb' WHERE file_id = (select MAX(file_id) FROM files) RETURNING file_id`)
 		client.query(`UPDATE files SET url = '${req.file.location}' WHERE file_id = (select MAX(file_id) FROM files) `)
 		    .then(result => {
 			res.status(200).json(result);
 		    })
 		    .catch(e => {
-			console.error(e), res.send(e);
+			console.error(e.detail), res.send(e);
 		    });
-		// Save the file name into database into profile model
-		// res.json({
-		//     url
-		// });
 	    }
 	}
     });
