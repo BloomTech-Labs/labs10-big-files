25d9f5d67a213aaef6856dd8c85f5d48db141a7e
K <kkhaag@gmail.com>
2019-02-27 11:05:30 -0800

    createedit was causing probs

--- Frontend/share_big_files/src/views/createedit.js
+++ Frontend/share_big_files/src/views/createedit.js
@@ -1,85 +1,85 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { FilePond } from "react-filepond";
-import "filepond/dist/filepond.min.css";
-// import { Link } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+// import { FilePond } from "react-filepond";
+// import "filepond/dist/filepond.min.css";
+// // import { Link } from "react-router-dom";
 
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import Axios from "axios";
-const CreateEditDiv = styled.div`
-  padding-left: 2%;
-  padding-right: 2%;
-  display: flex;
-  flex-direction: column;
-  height: 40rem;
-  width: 31rem;
-  margin-left: 4%;
-  border: 2px solid red;
-  line-height: 3;
-  background-color: white;
-  border-radius: 10%;
-`;
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+// import Axios from "axios";
+// const CreateEditDiv = styled.div`
+//   padding-left: 2%;
+//   padding-right: 2%;
+//   display: flex;
+//   flex-direction: column;
+//   height: 40rem;
+//   width: 31rem;
+//   margin-left: 4%;
+//   border: 2px solid red;
+//   line-height: 3;
+//   background-color: white;
+//   border-radius: 10%;
+// `;
 
-const CreateFileHolder = styled.div``;
+// const CreateFileHolder = styled.div``;
 
-const FileName = styled.input`
-  margin-left: 1%;
-`;
-const SharedWithBox = styled.div`
-  width: 100%;
-  height: 7rem;
-  border: 1px solid #a8a8a8;
-`;
+// const FileName = styled.input`
+//   margin-left: 1%;
+// `;
+// const SharedWithBox = styled.div`
+//   width: 100%;
+//   height: 7rem;
+//   border: 1px solid #a8a8a8;
+// `;
 
-const UploadButtonHolder = styled.div``;
-const ShareLinkHolder = styled.div``;
-const VersionBrowserHolder = styled.div``;
-const ConfirmButtons = styled.div``;
+// const UploadButtonHolder = styled.div``;
+// const ShareLinkHolder = styled.div``;
+// const VersionBrowserHolder = styled.div``;
+// const ConfirmButtons = styled.div``;
 
-const CreateFile = () => {
-  const handleInit = () => {
-    console.log("FilePond instance has initialised", this.pond);
-  };
-  return (
-    <CreateEditDiv>
-      <CreateFileHolder>
-        <span>File Name: </span>
-        <FileName type="text" placeholder="Name" />
-        <br />
-        <span>Share with:</span>
-        <FileName type="text" placeholder="Comma separate emails" />
-        <br />
+// const CreateFile = () => {
+//   const handleInit = () => {
+//     console.log("FilePond instance has initialised", this.pond);
+//   };
+//   return (
+//     <CreateEditDiv>
+//       <CreateFileHolder>
+//         <span>File Name: </span>
+//         <FileName type="text" placeholder="Name" />
+//         <br />
+//         <span>Share with:</span>
+//         <FileName type="text" placeholder="Comma separate emails" />
+//         <br />
 
-        <span>Shared with history:</span>
+//         <span>Shared with history:</span>
 
-        <SharedWithBox />
-      </CreateFileHolder>
-      <UploadButtonHolder>
-        <br />
-        {/* <input type="file" id="myFile" /> */}
+//         <SharedWithBox />
+//       </CreateFileHolder>
+//       <UploadButtonHolder>
+//         <br />
+//         {/* <input type="file" id="myFile" /> */}
 
-        <FilePond
-          allowMultiple={false}
-          maxFiles={1}
-          server="http://localhost:5000/api/s3/files/"
-        />
+//         <FilePond
+//           allowMultiple={false}
+//           maxFiles={1}
+//           server="http://localhost:5000/api/s3/files/"
+//         />
 
-        <br />
-      </UploadButtonHolder>
-      <ShareLinkHolder>
-        <h3>Share Link:</h3>
-      </ShareLinkHolder>
-      <VersionBrowserHolder>
-        <span>Version Browser: </span>
-        <FaArrowLeft size={15} className="fontAwesome" />
-        <FaArrowRight size={15} className="fontAwesome" />
-      </VersionBrowserHolder>
-      <ConfirmButtons>
-        <button>Cancel</button>
-        <button>Save</button>
-      </ConfirmButtons>
-    </CreateEditDiv>
-  );
-};
+//         <br />
+//       </UploadButtonHolder>
+//       <ShareLinkHolder>
+//         <h3>Share Link:</h3>
+//       </ShareLinkHolder>
+//       <VersionBrowserHolder>
+//         <span>Version Browser: </span>
+//         <FaArrowLeft size={15} className="fontAwesome" />
+//         <FaArrowRight size={15} className="fontAwesome" />
+//       </VersionBrowserHolder>
+//       <ConfirmButtons>
+//         <button>Cancel</button>
+//         <button>Save</button>
+//       </ConfirmButtons>
+//     </CreateEditDiv>
+//   );
+// };
 
-export default CreateFile;
+// export default CreateFile;
