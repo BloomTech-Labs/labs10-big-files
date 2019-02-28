445c642aaffed6f657cfc4b528679be82e9071ef
thomaskisner <tjkisner@gmail.com>
2019-02-28 13:39:39 -0500

    uncommented out imports that were accidentally commented out

--- Frontend/share_big_files/src/views/sharedholder.js
+++ Frontend/share_big_files/src/views/sharedholder.js
@@ -1,7 +1,7 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
-// import { FaShareSquare } from "react-icons/fa";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FaShareSquare } from "react-icons/fa";
 
 
  
