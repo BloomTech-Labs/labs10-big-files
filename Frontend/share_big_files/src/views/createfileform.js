54661b22fb9f3888d8c7c5ac684e14cb65a2cdc9
K <kkhaag@gmail.com>
2019-03-11 14:30:33 -0700

    changed the input form style

--- Frontend/share_big_files/src/views/createfileform.js
+++ Frontend/share_big_files/src/views/createfileform.js
@@ -36,6 +36,16 @@ const FileName = styled.input`
   border-bottom: 1px solid black;
   &:placeholder:
   color: black;
+
+  display: block;
+  border: none;
+  background: #e6e6e6;
+  font-size: 15px;
+  line-height: 1.5;
+
+  height: 50px;
+  border-radius: 25px;
+  padding: 0 0 0 3%;
 `;
 
 const FileNameMessage = styled.textarea`
@@ -46,6 +56,15 @@ const FileNameMessage = styled.textarea`
   &:placeholder: {
     color: blue;
   }
+
+  display: block;
+  border: none;
+  background: #e6e6e6;
+  font-size: 15px;
+  line-height: 1.5;
+
+  border-radius: 25px;
+  padding: 2% 0 0 3%;
 `;
 
 const InnerDiv = styled.div`
@@ -135,9 +154,6 @@ min-width: 270px
   background-color: #206db5;
   margin: 2.5% auto;
 `;
-const Form = styled.form`
-    // width: 500px;  
-`
 
 const CreateFileForm = () => {
   //const [link, setLink] = useState(null)
